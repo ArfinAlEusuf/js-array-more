1fa2f59b9ce448fef3b6135fa958382bc6b2ee73
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-30 01:20:08 +0600

    looping 20.5

---
+++ looping.js
@@ -0,0 +1,27 @@
+/**
+ * LOOPING Technique
+ * 1. for loop
+ * 2. wahile loop
+ * 3. do while 
+ * 4. for of 
+ * 5. for in
+ * 
+ */
+
+const friends =['elon','bill','mark', 'Waren']
+
+for( const friend of friends){
+    // console.log(friend);
+}
+
+for(let i=0; i <friends.length; i++){
+    // console.log(i);
+    // console.log(friends[i]);
+}
+
+
+const numbers= [5, 23,55,36,55,68,59,49,65,98,84]
+
+for(let i=0; i < numbers.length; i++){
+    console.log(numbers[i])
+}
